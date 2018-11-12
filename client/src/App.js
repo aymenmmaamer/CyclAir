fac5ee3de43112e3c1c559fb00f49705c95a6cfd
sxmxn <simonschuetze@me.com>
2018-11-12 09:46:49 +0100

    connect server with client

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: '',
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
   render() {
     return (
       <div className="App">
@@ -20,6 +34,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <p>{this.state.response}</p>
       </div>
     );
   }
