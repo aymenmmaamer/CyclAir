ada5edfb2d05462b3172b08efad5cab082186e3d
sxmxn <simonschuetze@me.com>
2018-12-08 19:37:07 +0100

    use google autocomplete libary and change Form component

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,18 +1,43 @@
-//import React from 'react';
-import React, { Component } from 'react'; // importieren von react componenten
-//export default Form;
+import React, { Component } from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 
-export default class Form extends React.Component { // export the expention function + function of Form.js
-    state = {
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
         startAdress: '',
-        destinationAdress: ' ',
+        destinationAdress: '',
+        suggestion: '',
+    }
     };
 
-    change = e => {
+    changeStart = e => {
         this.setState({
-            [e.target.name]: e.target.value
+            startAdress: e
         });
     };
+
+  changeDestination = e => {
+    this.setState({
+      destinationAdress: e
+    });
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };
+
+  handleEnter = address => {
+    this.setState({
+      suggestion: address
+    });
+  };
 
     onSubmit = (e) => {
         e.preventDefault(); 
@@ -25,24 +50,86 @@ export default class Form extends React.Component { // export the expention func
 
     render() {
         return (
-            <form>
-                <input 
-                    name = 'startAdress'
-                    placeholder ='Start Adresse'
-                    value={this.state.startAdress} 
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input 
-                    name = 'destinationAdress'
-                    type='text'
-                    placeholder='Ziel Adresse' //placeholder does not work
-                    value={this.state.destinationAdress} 
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <button onClick = {e => this.onSubmit(e)}>Submit</button>
-            </form>
+          <div>
+            <PlacesAutocomplete
+              value={this.state.startAdress}
+              onChange={e => this.changeStart(e)}
+              onSelect={this.handleSelect & this.handleEnter}
+            >
+              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <div>
+                  <input value={this.state.suggestion}
+                    {...getInputProps({
+                      placeholder: 'Startpunkt',
+                      className: 'location-search-input',
+                    })}
+                  />
+                  <div className="autocomplete-dropdown-container">
+                    {loading && <div>Loading...</div>}
+                    {suggestions.map(suggestion => {
+                      const className = suggestion.active
+                        ? 'suggestion-item--active'
+                        : 'suggestion-item';
+                      // inline style for demonstration purpose
+                      const style = suggestion.active
+                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                      return (
+                        <div
+                          {...getSuggestionItemProps(suggestion, {
+                            className,
+                            style,
+                          })}
+                        >
+                          <span>{suggestion.description}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </PlacesAutocomplete>
+
+            <PlacesAutocomplete
+              value={this.state.destinationAdress}
+              onChange={e => this.changeDestination(e)}
+              onSelect={this.handleSelect & this.handleEnter}
+            >
+              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <div>
+                  <input
+                    {...getInputProps({
+                      placeholder: 'Zielpunkt',
+                      className: 'location-search-input',
+                    })}
+                  />
+                  <div className="autocomplete-dropdown-container">
+                    {loading && <div>Loading...</div>}
+                    {suggestions.map(suggestion => {
+                      const className = suggestion.active
+                        ? 'suggestion-item--active'
+                        : 'suggestion-item';
+                      // inline style for demonstration purpose
+                      const style = suggestion.active
+                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                      return (
+                        <div
+                          {...getSuggestionItemProps(suggestion, {
+                            className,
+                            style,
+                          })}
+                        >
+                          <span>{suggestion.description}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </PlacesAutocomplete>
+            <button onClick = {e => this.onSubmit(e)}>Submit</button>
+          </div>
 
         );
 
